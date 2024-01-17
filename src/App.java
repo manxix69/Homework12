09ebb196fff8955a74caa5090f40c7c8334a762f
manxix69 <manxix69@gmail.com>
2024-01-17 23:39:49 +0500

    created methods in classes Book and Author ( toString(), equals(), hashCode() )

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) {
 
         Author author = new Author("Редьярд", "Киплинг");
-        Author author1 = new Author("Джорд", "Оруэлл");
+        Author author1 = new Author("Джордж", "Оруэлл");
 
         Book book = new Book("1984", author1, 1949);
         Book book1 = new Book("Скотный двор", author1, 1944);
@@ -16,6 +16,15 @@ public class App {
         book1.setYearOfPublication(1945);
 
         System.out.println("book1.getYearOfPublication() = " + book1.getYearOfPublication());
+
+
+        System.out.println("toString() = " +book);
+
+        System.out.println("isEquals? = " + book.getAuthor().equals(book1.getAuthor()));
+        System.out.println("book.getAuthor().hashCode() = " + (book.getAuthor().hashCode()));
+        System.out.println("book1.hashCode() = " + book1.hashCode());
+        book1.setYearOfPublication(1944);
+        System.out.println("book1.hashCode() = " + book1.hashCode());
 
     }
 }
